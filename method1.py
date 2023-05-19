0c15893d952e429d5a4b0f8b85c3ab8e0ea61bc8
harshupawar <133944443+harshupawar@users.noreply.github.com>
2023-05-19 10:56:44 +0530

    python u1

---
+++ method1.py
@@ -0,0 +1,18 @@
+
+def add(a,b):
+    c=a+b
+    return c
+
+def sub(a,b):
+    c=a-b
+    return c
+-
+def mul(a,b):
+    c=a*b
+    return c
+
+
+def div(a,b):
+    c=a/b
+    return c
+                     
